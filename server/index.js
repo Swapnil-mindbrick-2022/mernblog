d3066bbafee621873cf624b59bd1157df5ae0d64
Swapnil Bankar <swapnil@etgresearch.in>
2024-01-24 01:17:07 +0530

    add route, controller

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import mongoose from 'mongoose';
-
+import userRoute  from './routes/user.routes.js';
 dotenv.config()
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Mongodb connected ")
@@ -12,7 +12,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 const app = express();
 
 
-
+app.use('/api/user',userRoute )
 
 app.listen(3000,()=>{
     console.log("server is running at port 3000 !!!")
@@ -21,5 +21,5 @@ app.listen(3000,()=>{
 
 
 app.get('/test', (req,res)=>{
-    
+  
 })
