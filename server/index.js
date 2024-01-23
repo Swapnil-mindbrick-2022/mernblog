4f64e23f2f3f5f37a5f5c31e427d3190161c3f0f
Swapnil Bankar <swapnil@etgresearch.in>
2024-01-24 01:38:37 +0530

    create signup api route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 import mongoose from 'mongoose';
 import userRoute  from './routes/user.routes.js';
+import authRoute from './routes/auth.routes.js'
 dotenv.config()
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Mongodb connected ")
@@ -10,9 +11,11 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 
 const app = express();
+app.use(express.json())
 
 
 app.use('/api/user',userRoute )
+app.use('/api/user',authRoute )
 
 app.listen(3000,()=>{
     console.log("server is running at port 3000 !!!")
