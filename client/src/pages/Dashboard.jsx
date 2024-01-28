458ca891d5a243dfc398867c7bda45d4d51d6388
Swapnil Bankar <swapnil@etgresearch.in>
2024-01-29 01:18:06 +0530

    complete sidebar after daahboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,43 @@
 
 
+import { useEffect, useState } from 'react';
+import {useLocation} from 'react-router-dom'
+import DashSideBar from '../component/DashSidebaritem.jsx';
+import DashProfile from '../component/DashProfile.jsx';
+
 const Dashboard = () => {
+
+const location = useLocation();
+
+const [tab,setTab]= useState('');
+
+useEffect(() => {
+
+
+  const urlParams = new URLSearchParams(location.search)
+  const tabFromUrl = urlParams.get('tab');
+
+  // console.log(tabFromUrl)
+
+  if(tabFromUrl){
+    setTab(tabFromUrl);
+  }
+
+},[location.search])
+
+
+
   return (
-    <div>Dashboard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+    <div className=''>
+    {/*  SideBar*/}
+
+    <DashSideBar/>
+    </div>
+
+       {/*  PRofile*/}
+    {tab ==='profile' && <DashProfile/>}
+    </div>
   )
 }
 
